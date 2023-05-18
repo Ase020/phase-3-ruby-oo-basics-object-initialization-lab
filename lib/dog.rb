23d9aeb8404596552625a3e383056da11f182119
felixNyalenda <felixasejunior@gmail.com>
2023-05-18 15:16:16 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  attr_reader :breed, :name
+  def initialize(name,breed='Mutt')
+    @breed = breed
+    @name = name
+  end
+
+
+end
+
+bosco = Dog.new('Figo')
+
+puts bosco.breed, bosco.name
